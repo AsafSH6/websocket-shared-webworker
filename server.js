651bc06a9e06ec6f114be7c7ac91defadbe5b6fa
Asaf <asafsemail@gmail.com>
2021-05-14 23:22:29 +0300

    use express static to serve worker.js

--- server.js
+++ server.js
@@ -1,40 +1,36 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
 const morgan = require('morgan')
-const WebSocket = require('ws');
+const WebSocket = require('ws')
 
-const images = path.join(__dirname, 'images');
-const app = new express();
+
+const app = new express()
 app.use(morgan('combined'))
-
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(request, response){
-    response.sendFile('./index.html', { root: __dirname });
-});
-
-app.get('/worker.js', function(request, response){
-    response.sendFile('./worker.js', { root: __dirname });
+    response.sendFile('./index.html', { root: __dirname })
 });
 
 
-const wss = new WebSocket.Server({ port: 8080 });
-
-let index = 0;
+let index = 0
+const images = path.join(__dirname, 'images')
 const chunks = fs.readdirSync(images)
     .sort((a, b) => Number(a.replace('.jpg', '')) - Number(b.replace('.jpg', '')))
     .map(file => fs.readFileSync(path.join(images, file), {encoding: 'base64'}));
 
 
+const wss = new WebSocket.Server({ port: 8080 })
 wss.on('connection', function connection(ws) {
-    console.log('new websocket connection!');
+    console.log('new websocket connection!')
 
     ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
+        console.log('received: %s', message)
     });
 
     ws.on('close', function () {
-        console.log('connection closed!');
+        console.log('connection closed!')
     })
 });
 
@@ -42,11 +38,11 @@ wss.on('connection', function connection(ws) {
 setInterval(() => {
     wss.clients.forEach((client) => {
         index = index % chunks.length;
-        client.send(JSON.stringify({type: 'FRAME', content: chunks[index]}));
-        index++;
+        client.send(JSON.stringify({type: 'FRAME', content: chunks[index]}))
+        index++
     })
     // Video length is 53 seconds.
-}, (53 * 1000) / chunks.length);
+}, (53 * 100000) / chunks.length)
 
 
 app.listen(5000, () => {
